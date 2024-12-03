be099fd0bf2f0be54af5d99e158da92b6aeb1157
Zlybo <andres.gilnagles@gmail.com>
2024-12-03 11:10:09 -0500

    Styling home screen

--- app/login.js
+++ app/login.js
@@ -1,32 +1,33 @@
 import {Pressable, Text, View, TextInput} from "react-native";
 import {Link} from "expo-router";
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function Login() {
     return (
         <View className={"flex-1 bg-black"}>
-            <View className={"flex-row"}>
-                <MaterialIcons className={"ml-5"} name="notifications" size={32} color="gray"/>
+            <View className={"flex-row m-5"}>
+                <MaterialIcons name="notifications" size={32} color="gray"/>
                 <Text className={"flex-1 color-white font-medium text-center text-3xl"}>
                     Bienvenido
                 </Text>
-                <MaterialIcons className={"mr-5"} name="settings" size={32} color="gray"/>
+                <MaterialIcons name="settings" size={32} color="gray"/>
             </View>
-            <Text className={"color-white font-medium text-3xl ml-5 mt-5"}>
+            <Text className={"color-white font-medium text-3xl ml-5"}>
                 Username
             </Text>
-            <View className={"flex-row bg-[#1c1c1c] rounded-3xl items-center mx-5 mt-5"}>
-                <MaterialIcons className={"ml-5"} name="search" size={32} color="#FFEB3B"/>
+            <View className={"flex-row border border-gray-600 bg-[#1c1c1c] rounded-3xl items-center mx-5 mt-5"}>
                 <TextInput
-                    className={"color-white w-5/6 h-20 text-2xl"}
+                    className={"color-white w-5/6 h-16 text-xl ml-5"}
                     placeholder={"BUSCAR"} placeholderTextColor={"gray"}/>
+                <MaterialIcons name="search" size={32} color="#FFEB3B"/>
             </View>
             <Text className={"color-[##818180] text-3xl ml-5 mt-10"}>
-                Ultima visita
+                Última visita
             </Text>
 
-            <View className={"flex-row bg-[#1c1c1c] h-20 rounded-3xl mx-5 mt-5"}>
-                <View className={"ml-5 bg-white h-20 w-20"}></View>
+            <View className={"flex-row border border-gray-600 items-center bg-[#1c1c1c] h-20 rounded-3xl mx-5 mt-5"}>
+                <View className={"ml-5 bg-white rounded-2xl h-16 w-16"}></View>
                 <View className={"justify-center ml-5"}>
                     <Text className={"text-white"}>
                         NOMBRE APELLIDO
@@ -36,17 +37,23 @@ export default function Login() {
             </View>
 
             <Text className={"color-[##818180] text-3xl ml-5 mt-10"}>
-                Barberias cercanas
+                Barberías cercanas
             </Text>
 
-            <View className={"bg-[#1c1c1c] h-[250px] w-[200px] rounded-3xl mx-5 mt-5"}>
-                <View className={"ml-5 bg-white h-[150px] w-[150px]"}></View>
-                <Text className={"text-[#FFEB3B] ml-5"}>ABIERTA</Text>
-                <Text className={"text-white ml-5"}>Nombre de barberia</Text>
-                <View className={"ml-5 bg-[#fed60b] items-center justify-center rounded-3xl h-[40px] w-[150px]"}>
-                    <Text className={"font-bold text-xl"}>
-                        Agendar cita
-                    </Text>
+            <View
+                className={"bg-[#1c1c1c] h-[250px] w-[200px] border border-gray-600 items-center justify-center rounded-3xl ml-5 mt-5"}>
+                <View className={"bg-white rounded-2xl h-[150px] w-[150px]"}></View>
+                <Text className={"text-[#FFEB3B]"}>ABIERTA</Text>
+                <Text className={"text-white "}>Nombre de barbería</Text>
+                <View className={"flex-row justify-center items-center"}>
+                    <MaterialCommunityIcons
+                        className={"bg-[##2b2b2a] rounded-2xl p-3 mr-2"}
+                        name="bookmark-plus-outline" size={24} color="white"/>
+                    <View className={"bg-[#fed60b] items-center justify-center rounded-2xl h-[45.7px] w-[120px]"}>
+                        <Text className={"font-bold text-xl"}>
+                            Agendar cita
+                        </Text>
+                    </View>
                 </View>
             </View>
         </View>
